575f2659f2127bca7ad4869e298bfdff1c6c16d3
Tim Potter <tim@timpotter.name>
2025-02-24 18:08:32 -0600

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.timpotter.oeecalc"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
